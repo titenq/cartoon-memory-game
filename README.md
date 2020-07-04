f6a6bc603c48928aaacea6757288899fc5b3dcd0
titenq <titenq@gmail.com>
2020-07-04 11:46:09 -0300

    README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Jogo da Memória Cartoon
+![](https://img.shields.io/github/stars/titenq/cartoon-memory-game.svg) ![](https://img.shields.io/github/forks/titenq/cartoon-memory-game.svg) ![](https://img.shields.io/github/issues/titenq/cartoon-memory-game.svg) 
+
+### Jogo da Memória Cartoon
+
+### Demo
+[https://titenq.github.io/cartoon-memory-game/](https://titenq.github.io/cartoon-memory-game/ "https://titenq.github.io/cartoon-memory-game/")
+
+### Tecnologias
+- HTML
+- CSS
+- JavaScript
+
+### Screenshot
+
+![](https://github.com/titenq/cartoon-memory-game/blob/master/screenshot.png?raw=true)
+
+### Set Caverna do Dragão
+
+![](https://github.com/titenq/cartoon-memory-game/blob/master/set-caverna-do-dragao.png?raw=true)
+
+### Set He-Man
+
+![](https://github.com/titenq/cartoon-memory-game/blob/master/set-he-man.png?raw=true)
+
+### Set Thundercats
+
+![](https://github.com/titenq/cartoon-memory-game/blob/master/set-thundercats.png?raw=true)
--- assets/js/cardsThundercats.js
+++ assets/js/cardsThundercats.js
@@ -7,47 +7,47 @@ let cardsThundercats = [
   {
     id: 1,
     name: 'Escamoso',
-    src: './assets/thundercats/escamoso.jpg'
+    src: './assets/img/thundercats/escamoso.jpg'
   },
   {
     id: 2,
     name: 'Lion',
-    src: './assets/thundercats/lion.jpg'
+    src: './assets/img/thundercats/lion.jpg'
   },
   {
     id: 3,
     name: 'Mumm-Ra',
-    src: './assets/thundercats/mumm-ra.jpg'
+    src: './assets/img/thundercats/mumm-ra.jpg'
   },
   {
     id: 4,
     name: 'Panthro',
-    src: './assets/thundercats/panthro.jpg'
+    src: './assets/img/thundercats/panthro.jpg'
   },
   {
     id: 5,
     name: 'Simiano',
-    src: './assets/thundercats/simiano.jpg'
+    src: './assets/img/thundercats/simiano.jpg'
   },
   {
     id: 6,
     name: 'Snarf',
-    src: './assets/thundercats/snarf.jpg'
+    src: './assets/img/thundercats/snarf.jpg'
   },
   {
     id: 7,
     name: 'Tygra',
-    src: './assets/thundercats/tygra.jpg'
+    src: './assets/img/thundercats/tygra.jpg'
   },
   {
     id: 8,
     name: 'Willy Kat',
-    src: './assets/thundercats/willy-kat.jpg'
+    src: './assets/img/thundercats/willy-kat.jpg'
   },
   {
     id: 9,
     name: 'Willy Kit',
-    src: './assets/thundercats/willy-kit.jpg'
+    src: './assets/img/thundercats/willy-kit.jpg'
   },
 ];
 
