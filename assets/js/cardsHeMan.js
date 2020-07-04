79221b865b8a6f19b314be16a698541cf8152dfc
titenq <titenq@gmail.com>
2020-07-04 11:20:35 -0300

    paths

--- assets/js/cardsHeMan.js
+++ assets/js/cardsHeMan.js
@@ -2,52 +2,52 @@ let cardsHeMan = [
   {
     id: 0,
     name: 'Aquático',
-    src: '../../assets/img/he-man/aquatico.jpg'
+    src: './assets/img/he-man/aquatico.jpg'
   },
   {
     id: 1,
     name: 'Esqueleto',
-    src: '../../assets/img/he-man/esqueleto.jpg'
+    src: './assets/img/he-man/esqueleto.jpg'
   },
   {
     id: 2,
     name: 'Feiticeira',
-    src: '../../assets/img/he-man/feiticeira.jpg'
+    src: './assets/img/he-man/feiticeira.jpg'
   },
   {
     id: 3,
     name: 'Gato Guerreiro',
-    src: '../../assets/img/he-man/gato-guerreiro.jpg'
+    src: './assets/img/he-man/gato-guerreiro.jpg'
   },
   {
     id: 4,
     name: 'Gorpo',
-    src: '../../assets/img/he-man/gorpo.jpg'
+    src: './assets/img/he-man/gorpo.jpg'
   },
   {
     id: 5,
     name: 'He-Man',
-    src: '../../assets/img/he-man/he-man.jpg'
+    src: './assets/img/he-man/he-man.jpg'
   },
   {
     id: 6,
     name: 'Maligna',
-    src: '../../assets/img/he-man/maligna.jpg'
+    src: './assets/img/he-man/maligna.jpg'
   },
   {
     id: 7,
     name: 'Mentor',
-    src: '../../assets/img/he-man/mentor.jpg'
+    src: './assets/img/he-man/mentor.jpg'
   },
   {
     id: 8,
     name: 'She-Ra',
-    src: '../../assets/img/he-man/she-ra.jpg'
+    src: './assets/img/he-man/she-ra.jpg'
   },
   {
     id: 9,
     name: 'Teela',
-    src: '../../assets/img/he-man/teela.jpg'
+    src: './assets/img/he-man/teela.jpg'
   },
 ];
 
--- assets/js/cardsThundercats.js
+++ assets/js/cardsThundercats.js
@@ -2,52 +2,52 @@ let cardsThundercats = [
   {
     id: 0,
     name: 'Cheetara',
-    src: '../../assets/img/thundercats/cheetara.jpg'
+    src: './assets/img/thundercats/cheetara.jpg'
   },
   {
     id: 1,
     name: 'Escamoso',
-    src: '../../assets/thundercats/escamoso.jpg'
+    src: './assets/thundercats/escamoso.jpg'
   },
   {
     id: 2,
     name: 'Lion',
-    src: '../../assets/thundercats/lion.jpg'
+    src: './assets/thundercats/lion.jpg'
   },
   {
     id: 3,
     name: 'Mumm-Ra',
-    src: '../../assets/thundercats/mumm-ra.jpg'
+    src: './assets/thundercats/mumm-ra.jpg'
   },
   {
     id: 4,
     name: 'Panthro',
-    src: '../../assets/thundercats/panthro.jpg'
+    src: './assets/thundercats/panthro.jpg'
   },
   {
     id: 5,
     name: 'Simiano',
-    src: '../../assets/thundercats/simiano.jpg'
+    src: './assets/thundercats/simiano.jpg'
   },
   {
     id: 6,
     name: 'Snarf',
-    src: '../../assets/thundercats/snarf.jpg'
+    src: './assets/thundercats/snarf.jpg'
   },
   {
     id: 7,
     name: 'Tygra',
-    src: '../../assets/thundercats/tygra.jpg'
+    src: './assets/thundercats/tygra.jpg'
   },
   {
     id: 8,
     name: 'Willy Kat',
-    src: '../../assets/thundercats/willy-kat.jpg'
+    src: './assets/thundercats/willy-kat.jpg'
   },
   {
     id: 9,
     name: 'Willy Kit',
-    src: '../../assets/thundercats/willy-kit.jpg'
+    src: './assets/thundercats/willy-kit.jpg'
   },
 ];
 
