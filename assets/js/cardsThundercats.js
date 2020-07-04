2dcc9efc5d40b10b74d26d8d8fe69d7f6c44ea7b
titenq <titenq@gmail.com>
2020-07-04 11:22:35 -0300

    paths

--- assets/js/cardsThundercats.js
+++ assets/js/cardsThundercats.js
@@ -2,52 +2,52 @@ let cardsThundercats = [
   {
     id: 0,
     name: 'Cheetara',
-    src: './assets/img/thundercats/cheetara.jpg'
+    src: './cartoon-memory-game/assets/img/thundercats/cheetara.jpg'
   },
   {
     id: 1,
     name: 'Escamoso',
-    src: './assets/thundercats/escamoso.jpg'
+    src: './cartoon-memory-game/assets/thundercats/escamoso.jpg'
   },
   {
     id: 2,
     name: 'Lion',
-    src: './assets/thundercats/lion.jpg'
+    src: './cartoon-memory-game/assets/thundercats/lion.jpg'
   },
   {
     id: 3,
     name: 'Mumm-Ra',
-    src: './assets/thundercats/mumm-ra.jpg'
+    src: './cartoon-memory-game/assets/thundercats/mumm-ra.jpg'
   },
   {
     id: 4,
     name: 'Panthro',
-    src: './assets/thundercats/panthro.jpg'
+    src: './cartoon-memory-game/assets/thundercats/panthro.jpg'
   },
   {
     id: 5,
     name: 'Simiano',
-    src: './assets/thundercats/simiano.jpg'
+    src: './cartoon-memory-game/assets/thundercats/simiano.jpg'
   },
   {
     id: 6,
     name: 'Snarf',
-    src: './assets/thundercats/snarf.jpg'
+    src: './cartoon-memory-game/assets/thundercats/snarf.jpg'
   },
   {
     id: 7,
     name: 'Tygra',
-    src: './assets/thundercats/tygra.jpg'
+    src: './cartoon-memory-game/assets/thundercats/tygra.jpg'
   },
   {
     id: 8,
     name: 'Willy Kat',
-    src: './assets/thundercats/willy-kat.jpg'
+    src: './cartoon-memory-game/assets/thundercats/willy-kat.jpg'
   },
   {
     id: 9,
     name: 'Willy Kit',
-    src: './assets/thundercats/willy-kit.jpg'
+    src: './cartoon-memory-game/assets/thundercats/willy-kit.jpg'
   },
 ];
 
